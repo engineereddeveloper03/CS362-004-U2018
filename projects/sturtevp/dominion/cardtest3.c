894bfb23e7572a37ab9d7eef2d7390697a179dcf
engineereddeveloper03 <psturtevant400@gmail.com>
2018-07-22 21:42:58 -0700

    Added all 8 unit tests and modified Makefile to work with them.

---
+++ projects/sturtevp/dominion/cardtest3.c
@@ -0,0 +1,97 @@
+/* ----------------------------------------------------------------------
+ * Program Name: cardtest3.c
+ * Author: Phillip Sturtevant
+ * Date: July 22, 2018
+ * Description: A unit test for the Sea Hag Card in Dominion.
+ * ----------------------------------------------------------------------
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "string.h"
+
+int main() {
+   int seed = 1000;
+   int numPlayers = 2;
+   struct gameState G, testG;
+   int k[10] = { adventurer, council_room, feast, gardens, mine
+		, remodel, smithy, village, baron, great_hall };
+   
+   int player = 0;
+
+   // initialize a game state and player cards
+   initializeGame(numPlayers, k, seed, &G);
+   memcpy(&testG, &G, sizeof(struct gameState)); // copy game state
+
+   printf("TESTING Sea Hag Card:\n");
+
+   playSeaHagCard(&G);
+
+   printf("Check Player 2's deck to make sure they discarded a card and received curse.\n");
+
+   // Get pre and post deck counts
+   int deckPre1 = testG.deckCount[player]; 
+   int deckPre2 = testG.deckCount[1];
+   int deckPost1 = G.deckCount[player];
+   int deckPost2 = G.deckCount[1];
+
+   // Get pre and post hand counts
+   int handPre1 = testG.handCount[player];
+   int handPre2 = testG.handCount[1];
+   int handPost1 = G.handCount[player];
+   int handPost2 = G.handCount[1];
+
+   // Get pre and post discard piles
+   int discardPre1 = testG.discardCount[player];
+   int discardPre2 = testG.discardCount[1];
+   int discardPost1 = G.discardCount[player]; 
+   int discardPost2 = G.discardCount[1];
+
+   if(deckPre2 == deckPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Player 2 Deck Expected: %d  Result: %d\n", deckPre2, deckPost2);
+
+   printf("\nCheck Player 2 discard pile for added card from deck.\n");
+
+   if((discardPre2 + 1) == discardPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Player 2 Discard Expected: %d  Result: %d\n", (discardPre2 + 1), discardPost2);
+
+   printf("\nCheck Player 1 deck to ensure it is unchanged.\n");
+
+   if(deckPre1 == deckPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Player 1 Deck Expected: %d  Result: %d\n", deckPre1, deckPost1);
+
+   printf("\nCheck Player 1 discard pile and hand for change from dropping Sea Hag Card\n");
+
+   // Check Player's hand and discard for discarded card
+   if((discardPre1 + 1) == discardPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Player 1 Discard Expected: %d  Result: %d\n", (discardPre1 + 1), discardPost1);
+
+   if((handPre1 - 1) == handPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Player 1 Hand Expected: %d  Result: %d\n", (handPre1 - 1), handPost1);
+
+   printf("\nCheck Player 2 hand making sure it is unchanged.\n");
+
+   if(handPre2 == handPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Player 2 Hand Expected: %d  Result: %d\n", handPre2, handPost2);
+
+   printf("\nCheck curse cards are removed from the supply pile.\n");
+
+   int cursePre = testG.supplyCount[curse];
+   int cursePost = G.supplyCount[curse];
+
+   if((cursePre - 1) == cursePost) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Curses Expected: %d  Result: %d\n", (cursePre - 1), cursePost);
+
+   return 0;
+}
---
+++ projects/sturtevp/dominion/unittest2.c
@@ -0,0 +1,171 @@
+/* ----------------------------------------------------------------------
+ * Program Name: unittest2.c
+ * Author: Phillip Sturtevant
+ * Date: July 22, 2018
+ * Description: A unit test for the discardCard function in Dominion.
+ * ----------------------------------------------------------------------
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "string.h"
+
+int main() {
+   int i;
+   int seed = 1000;
+   int numPlayers = 2;
+   struct gameState G, testG;
+   int k[10] = { adventurer, council_room, feast, gardens, mine
+		, remodel, smithy, village, baron, great_hall };
+  
+   int trashFlag; 
+   int handPos = 1;
+   int player = 0;
+
+   // initialize a game state and player cards
+   initializeGame(numPlayers, k, seed, &G);
+
+   printf("TESTING discardCard function:\n");
+
+   trashFlag = 1;
+
+   printf("Checking if card is properly trashed\n");   
+   memcpy(&testG, &G, sizeof(struct gameState)); // copy game state   
+   discardCard(handPos, player, &G, trashFlag);
+   
+      int deckPre1 = testG.deckCount[player]; 
+      int deckPre2 = testG.deckCount[1];
+      int handPre1 = testG.handCount[player];
+      int handPre2 = testG.handCount[1];
+      int discardPre1 = testG.discardCount[player];
+      int discardPre2 = testG.discardCount[1];
+      int playedPre1 = G.playedCardCount;
+
+      int deckPost1 = G.deckCount[player];
+      int deckPost2 = G.deckCount[1];
+      int handPost1 = G.handCount[player];
+      int handPost2 = G.handCount[1];
+      int discardPost1 = G.discardCount[player]; 
+      int discardPost2 = G.discardCount[1];
+   int playedPost1 = G.playedCardCount;
+
+   printf("Player 1:\n");
+   if(discardPre1 == discardPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Discard Expected: %d  Result: %d\n", discardPre1, discardPost1);
+
+   if(deckPre1 == deckPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Deck Expected: %d  Result: %d\n", deckPre1, deckPost1);
+
+   if((handPre1 - 1) == handPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Hand Expected: %d  Result: %d\n", (handPre1 - 1), handPost1);
+
+   printf("Player 2:\n");
+   if(discardPre2 == discardPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Discard Expected: %d  Result: %d\n", discardPre2, discardPost2);
+
+   if(deckPre2 == deckPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Deck Expected: %d  Result: %d\n", deckPre2, deckPost2);
+
+   if(handPre2 == handPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Hand Expected: %d  Result: %d\n", handPre2, handPost2);
+
+   if(playedPre1 == playedPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Played Cards Expected: %d  Result: %d\n", playedPre1, playedPost1);
+
+   printf("\nChecking if not trashed, the card is in the played pile.\n");
+
+   memset(&G, 23, sizeof(struct gameState));   // clear the game state
+   initializeGame(numPlayers, k, seed, &G); // initialize a new game
+   memcpy(&testG, &G, sizeof(struct gameState)); // copy game state
+   trashFlag = 0;
+
+   discardCard(handPos, player, &G, trashFlag);
+
+   deckPre1 = testG.deckCount[player]; 
+   handPre1 = testG.handCount[player];
+   discardPre1 = testG.discardCount[player];
+   playedPre1 = testG.playedCardCount;
+
+   deckPost1 = G.deckCount[player];
+   handPost1 = G.handCount[player];
+   discardPost1 = G.discardCount[player]; 
+   playedPost1 = G.playedCardCount;
+
+   printf("Player 1:\n");
+   if(discardPre1 == discardPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Discard Expected: %d  Result: %d\n", discardPre1, discardPost1);
+
+   if(deckPre1 == deckPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Deck Expected: %d  Result: %d\n", deckPre1, deckPost1);
+
+   if((handPre1 - 1) == handPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Hand Expected: %d  Result: %d\n", (handPre1 - 1), handPost1);
+
+   printf("Player 2:\n");
+   if(discardPre2 == discardPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Discard Expected: %d  Result: %d\n", discardPre2, discardPost2);
+
+   if(deckPre2 == deckPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Deck Expected: %d  Result: %d\n", deckPre2, deckPost2);
+
+   if(handPre2 == handPost2) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Hand Expected: %d  Result: %d\n", handPre2, handPost2);
+
+   if((playedPre1 + 1) == playedPost1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Played Cards Expected: %d  Result: %d\n", (playedPre1 + 1), playedPost1);
+
+   printf("\nChecking if replaced card is last in hand.\n");
+
+   int lastCard = testG.hand[player][(testG.handCount[player] - 1)];
+   
+   if (lastCard == G.hand[player][handPos]) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Expected Card: %d  Result: %d\n", lastCard, G.hand[player][handPos]);
+   
+   printf("\nIf last card in hand, only remove it and don't set other cards.\n");
+
+   memset(&G, 23, sizeof(struct gameState));   // clear the game state
+   initializeGame(numPlayers, k, seed, &G); // initialize a new game
+   memcpy(&testG, &G, sizeof(struct gameState)); // copy game state
+
+   // remove all cards from hand except 1
+   for (i = 1; i < 5; i++)
+   {
+      G.hand[player][i] = -1;
+   }
+   G.handCount[player] = 1;
+   handPos = 0;
+
+   discardCard(handPos, player, &G, trashFlag);
+
+   int cardInHand = G.hand[player][handPos];
+   int totalCards = G.handCount[player];
+
+   if (cardInHand == -1) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Card Enum Expected: -1  Result: %d\n", cardInHand);
+
+   if (totalCards == 0) { printf("TEST SUCCESS\n"); }
+   else { printf("TEST FAILURE\n"); }
+   printf("Total Cards Expected: 0  Result: %d\n", totalCards);
+
+   return 0;
+}
